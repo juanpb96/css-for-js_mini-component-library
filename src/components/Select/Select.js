88c1e8cc0a361651eb510bcff92a530c344cf71d
MasterDev <juanpb.96@gmail.com>
2024-02-03 13:23:38 -0500

    Add initial structure and styles for custom select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,33 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  padding: 12px 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+`;
+
+const DefaultSelect = styled.select`
+  display: none;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <p>{displayedValue}</p>
+      <Icon id="chevron-down" />
+      <DefaultSelect value={value} onChange={onChange}>
+        {children}
+      </DefaultSelect>
+    </Wrapper>
   );
 };
 
