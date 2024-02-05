7e9544e0a5ab56c155f2a39a2e25b375fc2d59d3
MasterDev <juanpb.96@gmail.com>
2024-02-05 12:11:12 -0500

    Apply styles for default and custom select plus a11y adjustments

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+import VisuallyHidden from '../VisuallyHidden';
 import { getDisplayedValue } from './Select.helpers';
 
 const Wrapper = styled.div`
   width: fit-content;
+  position: relative;
   display: flex;
   align-items: center;
   gap: 24px;
@@ -14,21 +16,39 @@ const Wrapper = styled.div`
   border: none;
   border-radius: 8px;
   background-color: ${COLORS.transparentGray15};
-  color: ${COLORS.gray700};
+  color: var(--color);
+  font-size: 1rem;
+  line-height: 1.17;
+  outline: 1px solid transparent;
+
+  &:focus-within {
+    outline-color: ${COLORS.focus};
+  }
 `;
 
 const DefaultSelect = styled.select`
-  display: none;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  opacity: 0;
+  border-radius: inherit;
 `;
 
 const Select = ({ label, value, onChange, children }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper>
+    <Wrapper
+      style={{"--color": isHovered ? COLORS.black : COLORS.gray700}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <VisuallyHidden id="select-label">{label}</VisuallyHidden>
       <p>{displayedValue}</p>
-      <Icon id="chevron-down" />
-      <DefaultSelect value={value} onChange={onChange}>
+      <Icon id="chevron-down" strokeWidth={isHovered ? 2 : 1} />
+      <DefaultSelect aria-labelledby="select-label" value={value} onChange={onChange}>
         {children}
       </DefaultSelect>
     </Wrapper>
